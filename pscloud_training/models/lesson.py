8c4726803055ba02b821ca47e5c858d174dce13b
lujianweisa <lujianweisa@163.com>
2018-12-07 11:42:21 +0000

    luTest

--- pscloud_training/models/lesson.py
+++ pscloud_training/models/lesson.py
@@ -8,6 +8,6 @@ teacher_id = fields.Many2one('res.partner', string='老师', domain=[('is_teache
 student_ids = fields.Many2many('res.partner', string='学生', domain=[('is_student', '=', True)], readonly=True)
 start_date = fields.Date(string='开始时间')
 end_date = fields.Date(string='结束时间')
-    seat_qty = fields.Integer(string='座位数')
-    subject_id = fields.Many2one('pscloud.training.subject', string='科目')
+seat_qty = fields.Integer(string='座位数')
+subject_id = fields.Many2one('pscloud.training.subject', string='科目')
     
