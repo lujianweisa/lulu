50901cadae20801c4b47d9aafb7a32f2933d588e
lujianweisa <45680282+lujianweisa@users.noreply.github.com>
2018-12-08 08:04:23 +0000

    lulu

--- pscloud_training/models/.ipynb_checkpoints/subject-checkpoint.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-
-class TrainingSubject(models.Model):
- _name='pscloud_training.models.subject'
- _description='科目'
- name=fields.Char(string='名称')
- person_id=fields.Many2one('res_partner','负责人')
- lesson_ids = fields.One2many('pscloud_training.models.lesson', 'subject_id', string='课程')
- desc = fields.Text(string='描述')
--- pscloudtraininglulu/__manifest__.py
+++ pscloudtraininglulu/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'PS Cloud 培训',
+    'name': 'PS Cloud 培训 LU',
     'version': '12.0.1.0',
     'summary': 'PS Cloud 培训',
     'author': "www.mypscloud.com",
@@ -7,8 +7,9 @@
     'category': 'Tools',
     'depends': ['base'],
     'data': [
-        'views/training_subject_views.xml',
+        'views/res_partner_views.xml',
     ],
+    'demo': [],
     'qweb': [],
     'js': [],
     'css': [],
